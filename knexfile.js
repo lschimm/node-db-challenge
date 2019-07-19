12b2f3e1fbc87a859956498b337836665a3500e2
lschimm <lschimmelfennig@gmail.com>
2019-07-19 10:27:19 -0500

    database in sqlite, working on migrations

--- knexfile.js
+++ knexfile.js
@@ -1,44 +1,50 @@
 // Update with your config settings.
 
 module.exports = {
-
   development: {
-    client: 'sqlite3',
+    client: "sqlite3",
+    useNullAsDefault: true,
     connection: {
-      filename: './dev.sqlite3'
+      filename: "./data/projects.db3"
+    },
+    migrations: {
+      directory: "./data/migrations"
     }
+    //don't need seeds but just in case:
+    // seeds: {
+    //   directory: "./data/seeds"
+    // }
   },
 
   staging: {
-    client: 'postgresql',
+    client: "postgresql",
     connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
+      database: "my_db",
+      user: "username",
+      password: "password"
     },
     pool: {
       min: 2,
       max: 10
     },
     migrations: {
-      tableName: 'knex_migrations'
+      tableName: "knex_migrations"
     }
   },
 
   production: {
-    client: 'postgresql',
+    client: "postgresql",
     connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
+      database: "my_db",
+      user: "username",
+      password: "password"
     },
     pool: {
       min: 2,
       max: 10
     },
     migrations: {
-      tableName: 'knex_migrations'
+      tableName: "knex_migrations"
     }
   }
-
 };
